e7478a700b9f14b27dbe108e6b665ef0879a73c5
cecilepardo <pardo.cle@gmail.com>
2025-10-29 11:47:27 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  return <UserProfile />;
+}
+
+export default App;
---
+++ src/components/UserProfile.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function UserProfile() {
+  return (
+    <>
+      <p>User is </p>
+
+      <button>click to change user status </button>
+    </>
+  );
+}
+
+export default UserProfile;
